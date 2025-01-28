a23f56ed51dee4f7e249d44a8a8c232b574e3548
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2025-01-28 09:39:59 +0100

    Initial Commit

---
+++ AppleMusicLayout/AppleMusicLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicLayoutApp.swift
+//  AppleMusicLayout
+//
+//  Created by Grzegorz Mzyk on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
